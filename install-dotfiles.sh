a77b4605531f831ee96acdc1033703c674f6bae6
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-03-10 20:19:28 +0100

    Install nanorc

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# install "Improved Nano Syntax Highlighting Files" from
+# https://github.com/scopatz/nanorc
+curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
+
 cat .bash_aliases >> ~/.bash_aliases
 cat .bashrc >> ~/.bashrc
