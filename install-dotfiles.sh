664bf9df4779f7d3c528897a93b68d36aac21328
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-03-11 13:06:50 +0100

    Add git aliases

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -6,3 +6,8 @@ curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
 
 cat .bash_aliases >> ~/.bash_aliases
 cat .bashrc >> ~/.bashrc
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
