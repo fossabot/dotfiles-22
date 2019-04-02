a7179ef8d36d507c17d1c6314731cb145f8b97ec
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-04-02 11:28:16 +0200

    Add ttyd

--- install-programs.sh
+++ install-programs.sh
@@ -74,6 +74,14 @@ gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D695
 sudo usermod -aG rvm "${USER}"
 sudo usermod -aG rvm root
 
+# ttyd
+sudo apt install -y cmake libjson-c-dev libwebsockets-dev
+cd "$HOME/src/"
+git clone https://github.com/tsl0922/ttyd.git
+mkdir ttyd/build && cd ttyd/build
+cmake ..
+make && sudo make install
+
 # random utils
 sudo apt install mailutils tree unzip zip
 
