9c5a19be6327548ee3f72bcb9c2a0900a29fa14f
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-05-16 19:50:03 +0200

    Set git push to default to the current branch

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -11,4 +11,5 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+git config --global push.default current
 git config --global pull.rebase true
